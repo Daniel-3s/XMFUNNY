08aa2645b2adb18506a34b89cb9fb1e4b04a9b13
邱星晨 <843224210@qq.com>
2018-08-22 18:46:53 +0800

    default

---
+++ Assets/Script/ConverMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConverMove : MonoBehaviour {
+    public float speed;
+    public Transform start;
+    public Transform end;
+    public Vector3 s;
+    public Vector3 e;
+    public bool go;
+    public bool back;
+	// Use this for initialization
+	void Start () {
+        s = start.position;
+        e = end.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(start.position.y == s.y)
+        {
+            go = true;
+            back = false;
+        }
+        if(start.position.y == e.y)
+        {
+            back = true;
+            go = false;
+        }
+        if (go == true)
+        {
+            transform.position = Vector3.MoveTowards(start.position, e, speed * Time.deltaTime);
+        }
+        if (back == true)
+        {
+            transform.position = Vector3.MoveTowards(start.position, s, speed * Time.deltaTime);
+        }
+	}
+}
---
+++ Assets/Script/Radio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radio : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void CloseRadio()
+    {
+        GetComponent<Animator>().enabled = false;
+    }
+}
