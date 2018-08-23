79af30c6c4b9050f0aad47bd685f571cd0320c47
HarryDongH <1053514960@qq.com>
2018-08-23 10:31:32 +0800

    player attributes

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Update is called once per frame
-	void Update () {
+    private int blood = 100;
+    private int mood = 100;
+    private int hunger = 100;
+    public int Blood
+    {
+        get { return blood; }
+        set { blood = value; }
+    }
+    public int Mood
+    {
+        get { return mood; }
+        set { mood = value; }
+    }
+    public int Hunger
+    {
+        get { return hunger; }
+        set { hunger = value; }
+    }
+    // Update is called once per frame
+    void Update () {
         if (Input.GetKeyDown(KeyCode.G))
         {
             int id = Random.Range(1, 3);
